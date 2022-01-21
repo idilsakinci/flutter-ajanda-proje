e4be95311e6c856da89bfb1c9d53f3e474200e36
İdil Sakıncı <76444215+idilsakinci@users.noreply.github.com>
2022-01-21 22:04:23 +0300

    Update create_account_form.dart

--- ajanda_flutter/lib/widgets/create_account_form.dart
+++ ajanda_flutter/lib/widgets/create_account_form.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// yeni kullanıcı oluşturma
-
 class CreateAccountForm extends StatelessWidget {
   const CreateAccountForm({
     Key? key,
